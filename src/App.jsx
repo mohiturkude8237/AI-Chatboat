146200adef8af75f82185a4053801b0d57f9daac
mohiturkude8237 <mohiturkude8237@gmail.com>
2025-07-02 17:36:21 +0530

    App.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Mainchatbot from './components/chatbot/mainchatbot';
-import Home from "./components/HomePage/HomePage";
+import HomePage from "./components/HomePage/HomePage";
 import About from "./components/About";
 import Features from "./components/Features";
 import Contact from "./components/Contact";
